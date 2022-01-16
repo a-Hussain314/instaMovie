1158fe6bc713a4ec564a27f6eb9ed0a03e849fcc
Ahmed Hussain <ah0240089@gmail.com>
2022-01-16 10:59:36 +0200

    add network connection listener, to use before fetching any movies

--- src/components/movieLists/AllMovies.js
+++ src/components/movieLists/AllMovies.js
@@ -1,6 +1,7 @@
 import React, {useCallback, useEffect, useRef, useState} from 'react';
-import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, Alert, StyleSheet, Text, View} from 'react-native';
 import {getMovies} from '../../utils/requester';
+import NetInfo from '@react-native-community/netinfo';
 import commonStyles from '../../style/commonStyles';
 import MoviesList from '../MoviesList';
 
@@ -9,22 +10,49 @@ const AllMovies = () => {
   const [allMovies, setAllMovies] = useState([]);
   const pageNumber = useRef(1);
 
+  useEffect(() => {
+    // on app mount, attach network connection listener
+    NetInfo.addEventListener(network => {
+      // in case of conection restored and first page not loaded yet
+      if (network.isConnected && !AllMovies.length) {
+        loadMovies();
+      } else {
+        alertNoNetworkConnection();
+      }
+    });
+  }, []);
+
   const loadMovies = useCallback(() => {
-    setIsloading(true); // show the loading spinner
-    // fetch the first/next page movies 
-    getMovies({
-      pageNumber: pageNumber.current,
-      onSuccess: data => {
-        pageNumber.current++; // update page number value to fetch next page in next function call
-        setIsloading(false); // hide the loading spinner
-        setAllMovies([...allMovies, ...data]); // update "allmovies" list/state to add the newely fetched movies
-      },
+    // check internet connection first
+    NetInfo.fetch().then(network => {
+      if (network.isConnected) {
+        // in case of network connected.
+        if (!isLoading) {
+          setIsloading(true); // show the loading spinner
+        }
+        // fetch the first/next page movies
+        getMovies({
+          pageNumber: pageNumber.current,
+          onSuccess: data => {
+            pageNumber.current++; // update page number value to fetch next page in next function call
+            setIsloading(false); // hide the loading spinner
+            setAllMovies([...allMovies, ...data]); // update "allmovies" list/state to add the newely fetched movies
+          },
+        });
+      } else {
+        // in case of network disconnected.
+        alertNoNetworkConnection();
+      }
     });
   }, [setIsloading, allMovies, setAllMovies, pageNumber.current]);
 
-  useEffect(() => {
-    loadMovies();
-  }, []);
+  const alertNoNetworkConnection = useCallback(() => {
+    setIsloading(false);
+    Alert.alert(
+      'No internet connection',
+      'Unable to load movies, plaese check your internet connection and try again.',
+    );
+  }, [setIsloading]);
 
   return (
     <View style={styles.container}>
